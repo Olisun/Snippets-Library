f661ae56330568b8c34472805975947e1430f1c6
Oliver Sun <olisun@mac.com>
2019-09-11 14:22:31 -0700

    adding firebase credentials

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Bootcamp
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAnfB4KI8rWLaQfRuf3umQlPVw-xreonvw",
+  authDomain: "bootcamp-266ad.firebaseapp.com",
+  databaseURL: "https://bootcamp-266ad.firebaseio.com",
+  projectId: "bootcamp-266ad",
+  storageBucket: "bootcamp-266ad.appspot.com",
+  messagingSenderId: "425399544190",
+  appId: "1:425399544190:web:6c28aa81b3e2e624082b2f"
+};
+firebase.initializeApp(config);
+
+// VARIABLES
+// --------------------------------------------------------------------------------
+
+var database = firebase.database();
